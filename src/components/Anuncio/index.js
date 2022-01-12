08aef5260fc3036d0bcd54a78fa5ec9c7c98cdb1
luiz fernando <darkloveaj23@gmail.com>
2022-01-12 10:22:36 -0300

    anuncios

--- src/components/Anuncio/index.js
+++ src/components/Anuncio/index.js
@@ -5,9 +5,10 @@ import './styled.css';
 const Anuncio = () => {
   return (
     <iframe
-      src="//www.effectivedisplaycontent.com/watchnew?key=fec29dbebffcde2959861237fca1c4df "
+      src="//www.effectivedisplaycontent.com/fec29dbebffcde2959861237fca1c4df/invoke "
       frameBorder="0"
       scrolling="no"
+      className="anuncio"
     />
   );
 };
