928d2fef9e88f70dd6fe20a755dd7379e0b7fc13
luiz fernando <darkloveaj23@gmail.com>
2022-01-12 11:11:22 -0300

    anuncios

--- src/components/Anuncio/index.js
+++ src/components/Anuncio/index.js
@@ -3,9 +3,9 @@ import React from 'react';
 import './styled.css';
 
 const frame =
-  'www.effectivedisplaycontent.com/watchnew?key=fec29dbebffcde2959861237fca1c4df';
+  '//www.effectivedisplaycontent.com/watchnew?key=fec29dbebffcde2959861237fca1c4df/invoke.js';
 const Anuncio = () => {
-  return <iframe src={frame} />;
+  return <iframe className="anuncio" src={frame} />;
 };
 
 export default Anuncio;
